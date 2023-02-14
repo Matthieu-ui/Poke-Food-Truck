a51c702080b7768c6bff18995fc54686aaa01106
Isaac J <duckhunterjr@gmail.com>
2023-02-13 20:51:22 -0800

    add initial tailwind menu component

--- includes/header.php
+++ includes/header.php
@@ -4,10 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="styles/styles.css">
+     <!-- <link rel="stylesheet" href="styles/styles.css"> -->
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
 <body>
-
-<h1 class="text-2xl text-blue-500">POKE TRUCK</h1>
-
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include('includes/header.php');
+include 'includes/header.php';
 
 /*
 Need to figure out
@@ -82,175 +82,218 @@ $items = new Item(
     14.95
 );
 $menuItems[] = $items;
-
 ?>
-
     
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-    <div class="card-container">
+    
+    <div class="mt-8 bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+        <h1 class="text-2xl text-blue-500">POKE TRUCK</h1>
         <?php //grabs $menuItem array -> sets item
         foreach ($menuItems as $key => $item) { ?>
-        <div class="card">
-            <h2><?= $item->name ?></h2>
-            <p><?= $item->description ?></p>
-            <label>Quantity</label>
-            <!-- check if selected -->
-            <select name="quantity-<?= $key ?>">
-                <option value="0" <?php if (isset($_POST['quantity-' . $key]) && $_POST['quantity-' . $key] == 0) {
-                    echo 'selected="selected"';
-                } ?>>0</option>
-                <option value="1" <?php if (isset($_POST['quantity-' . $key]) && $_POST['quantity-' . $key] == 1) {
-                    echo 'selected="selected"';
-                } ?>>1</option>
-                <option value="2" <?php if (isset($_POST['quantity-' . $key]) && $_POST['quantity-' . $key] == 2) {
-                    echo 'selected="selected"';
-                } ?>>2</option>
-                <option value="3" <?php if (isset($_POST['quantity-' . $key]) && $_POST['quantity-' . $key] == 3) {
-                    echo 'selected="selected"';
-                } ?>>3</option>
-                <option value="4" <?php if (isset($_POST['quantity-' . $key]) && $_POST['quantity-' . $key] == 4) {
-                    echo 'selected="selected"';
-                } ?>>4</option>
-                <option value="5" <?php if (isset($_POST['quantity-' . $key]) && $_POST['quantity-' . $key] == 5) {
-                    echo 'selected="selected"';
-                } ?>>5</option>
+        <div class="my-8">
+            <div class="flex justify-between">
+            <h2 class=""><?= $item->name ?></h2>
+            <div>
+                <label class="">Quantity</label>
+                <!-- check if selected -->
+                <select name="quantity-<?= $key ?>">
+                    <option value="0" <?php if (
+                        isset($_POST['quantity-' . $key]) &&
+                        $_POST['quantity-' . $key] == 0
+                    ) {
+                        echo 'selected="selected"';
+                    } ?>>0</option>
+                    <option value="1" <?php if (
+                        isset($_POST['quantity-' . $key]) &&
+                        $_POST['quantity-' . $key] == 1
+                    ) {
+                        echo 'selected="selected"';
+                    } ?>>1</option>
+                    <option value="2" <?php if (
+                        isset($_POST['quantity-' . $key]) &&
+                        $_POST['quantity-' . $key] == 2
+                    ) {
+                        echo 'selected="selected"';
+                    } ?>>2</option>
+                    <option value="3" <?php if (
+                        isset($_POST['quantity-' . $key]) &&
+                        $_POST['quantity-' . $key] == 3
+                    ) {
+                        echo 'selected="selected"';
+                    } ?>>3</option>
+                    <option value="4" <?php if (
+                        isset($_POST['quantity-' . $key]) &&
+                        $_POST['quantity-' . $key] == 4
+                    ) {
+                        echo 'selected="selected"';
+                    } ?>>4</option>
+                    <option value="5" <?php if (
+                        isset($_POST['quantity-' . $key]) &&
+                        $_POST['quantity-' . $key] == 5
+                    ) {
+                        echo 'selected="selected"';
+                    } ?>>5</option>
 
-            </select>
-
-
-            <div class="addon">
-                <!-- check if checked -->
-                <label for="addon">Add Ons</label>
-                <input type="checkbox" name="addon-<?= $key ?>[]" value="tuna" <?php if (isset($_POST['addon-' . $key]) && in_array('tuna', $_POST['addon-' . $key])) {
-                    echo 'checked="checked"';
-                } ?>>
+                </select>
+            </div>
+            </div>
+                <p class="my-2"><?= $item->description ?></p>
             
+
+            
+                <!-- check if checked -->
+                <div class="flex justify-between">
+
+                
+                <label for="addon">Extras: </label>
+                <div class="w-5/6 ">
+                
+                <div>
+                <input type="checkbox" name="addon-<?= $key ?>[]" value="tuna" <?php if (
+    isset($_POST['addon-' . $key]) &&
+    in_array('tuna', $_POST['addon-' . $key])
+) {
+    echo 'checked="checked"';
+} ?>>
                 <label for="addon">Add Tuna</label>
-                <input type="checkbox" name="addon-<?= $key ?>[]" value="rice"<?php if (isset($_POST['addon-' . $key]) && in_array('rice', $_POST['addon-' . $key])) {
-                    echo 'checked="checked"';
-                }?>>
+                </div>
+
+                <div>
+                <input type="checkbox" name="addon-<?= $key ?>[]" value="rice"<?php if (
+    isset($_POST['addon-' . $key]) &&
+    in_array('rice', $_POST['addon-' . $key])
+) {
+    echo 'checked="checked"';
+} ?>>
                 <label for="addon">Add Rice</label>
-                <input type="checkbox" name="addon-<?= $key ?>[]" value="onion" <?php if (isset($_POST['addon-' . $key]) && in_array('onion', $_POST['addon-' . $key])) {
-                    echo 'checked="checked"';
-                }?>>
+                </div>
+
+                <div>
+                <input type="checkbox" name="addon-<?= $key ?>[]" value="onion" <?php if (
+    isset($_POST['addon-' . $key]) &&
+    in_array('onion', $_POST['addon-' . $key])
+) {
+    echo 'checked="checked"';
+} ?>>
                 <label for="addon">Add Onions</label>
+                </div>
+                </div>
             </div>     
         </div>
 
 
         <?php } ?>
-    </div>
-    <button type="submit">Submit</button>
+        <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"type="submit">Submit</button>
     <!-- php self reset -->
-    <button type="reset"  onClick="window.location.href='<?php echo $_SERVER['PHP_SELF'] ?>'" value="Reset">Reset</button>
+    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" type="reset"  onClick="window.location.href='<?php echo $_SERVER[
+        'PHP_SELF'
+    ]; ?>'" value="Reset">Reset</button>
+    </div>
+    
         <br>
     </form>
     <?php
-
     //if post request***
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    //loop through menu items
-    foreach ($menuItems as $key => $item) {
-        //if quantity is not empty
-        if (!empty($_POST['quantity-' . $key])) {
-            //set quantity
-            $item->setQuant($_POST['quantity-' . $key]);
-            //set addon if not empty
-            if (!empty($_POST['addon-' . $key])) {
-                $item->setAddon($_POST['addon-' . $key]);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        //loop through menu items
+        foreach ($menuItems as $key => $item) {
+            //if quantity is not empty
+            if (!empty($_POST['quantity-' . $key])) {
+                //set quantity
+                $item->setQuant($_POST['quantity-' . $key]);
+                //set addon if not empty
+                if (!empty($_POST['addon-' . $key])) {
+                    $item->setAddon($_POST['addon-' . $key]);
+                }
+                //add to cart
+                $cart[] = $item;
             }
-            //add to cart
-            $cart[] = $item;
+        } //end foreach
+    }
+
+    class Cart
+    {
+        public $items = [];
+        public $addons = [];
+        public $subtotal = 0;
+        public $tax = 0;
+        public $total = 0;
+
+        public function __construct($items)
+        {
+            $this->items = $items;
         }
-    } //end foreach
-}
-
-
-
-class Cart
-{
-    public $items = [];
-    public $addons = [];
-    public $subtotal = 0;
-    public $tax = 0;
-    public $total = 0;
-
-
-    public function __construct($items)
-    {
-        $this->items = $items;
-    }
-    public function setAddon($addon)
-    {
-        $this->addons = $addon;
-    }
-
-    public function subtotal()
-    {
-        foreach ($this->items as $item) {
-            $this->subtotal += $item->price * $item->quantity + count($item->addon) * 1.50;
+        public function setAddon($addon)
+        {
+            $this->addons = $addon;
         }
-        return number_format($this->subtotal, 2);
-    }
 
-    public function tax()
-    {
-        $this->tax = $this->subtotal * .10;
-        return number_format($this->tax, 2);
-    }
-
-    public function total()
-    {
-        $this->total = $this->subtotal + $this->tax;
-        return number_format($this->total, 2);
-    }
-
-    public function displayCart()
-    // addons is array
-    {
-        foreach ($this->items as $item) {
-            echo $item->quantity . ' ';
-            echo $item->name . ' $' . $item->price . '';
-            echo ' <br/> Add Ons: ';
-            // implode array (array to string) and add $1.50 to each
-            echo implode(', ', $item->addon) . ' $' . number_format(count($item->addon) * 1.50, 2) ;
-            // if no addon
-            if (empty($item->addon)) {
-                echo ' None';
+        public function subtotal()
+        {
+            foreach ($this->items as $item) {
+                $this->subtotal +=
+                    $item->price * $item->quantity + count($item->addon) * 1.5;
             }
+            return number_format($this->subtotal, 2);
+        }
 
-            echo '<br>';
+        public function tax()
+        {
+            $this->tax = $this->subtotal * 0.1;
+            return number_format($this->tax, 2);
+        }
+
+        public function total()
+        {
+            $this->total = $this->subtotal + $this->tax;
+            return number_format($this->total, 2);
+        }
+
+        public function displayCart()
+        {
+            // addons is array
+            foreach ($this->items as $item) {
+                echo $item->quantity . ' ';
+                echo $item->name . ' $' . $item->price . '';
+                echo ' <br/> Add Ons: ';
+                // implode array (array to string) and add $1.50 to each
+                echo implode(', ', $item->addon) .
+                    ' $' .
+                    number_format(count($item->addon) * 1.5, 2);
+                // if no addon
+                if (empty($item->addon)) {
+                    echo ' None';
+                }
+
+                echo '<br>';
+            }
+        }
+
+        public function displaySubtotal()
+        {
+            echo $this->subtotal();
         }
     }
 
-
-    public function displaySubtotal()
-    {
-        echo $this->subtotal();
+    //if cart is not empty display cart
+    if (!empty($cart)) {
+        echo '<h2>Cart</h2>';
+        $cart = new Cart($cart);
+        $cart->displayCart();
+        echo '<br>';
+        echo 'Subtotal: $';
+        $cart->displaySubtotal();
+        echo '<br>';
+        echo 'Tax: $';
+        echo $cart->tax();
+        echo '<br>';
+        echo 'Total: $';
+        echo $cart->total();
+    } else {
+        echo 'Your cart is empty!';
     }
-}
-
-//if cart is not empty display cart
-if (!empty($cart)) {
-    echo '<h2>Cart</h2>';
-    $cart = new Cart($cart);
-    $cart->displayCart();
-    echo '<br>';
-    echo 'Subtotal: $';
-    $cart->displaySubtotal();
-    echo '<br>';
-    echo 'Tax: $';
-    echo $cart->tax();
-    echo '<br>';
-    echo 'Total: $';
-    echo $cart->total();
-} else {
-    echo 'Your cart is empty!';
-}
-
-
-?>
+    ?>
 
     </body>
 
